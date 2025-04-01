8b098be59348ea1bca3bb44e7470dd219b281e30
Yulia_Smiyan <143211897+Difraya@users.noreply.github.com>
2025-04-01 23:26:50 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.api_v1.api import api_router
-# Импортируем новый роутер для редиректов
 from app.api.api_v1.endpoints.links import redirect_router
 
 app = FastAPI(
@@ -11,7 +10,6 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
-# Настройка CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,7 +18,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Подключаем роутер API
 app.include_router(api_router, prefix=settings.API_V1_STR)
-# Подключаем роутер редиректов в корень
 app.include_router(redirect_router) 
